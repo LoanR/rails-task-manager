49e790d3af7188fdbd64f2688e50a4a542bfbf59
Loan Robert <loan.rbrt@gmail.com>
2016-08-09 12:45:34 +0200

    updated routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+
+  # get 'tasks/index'
+
+  get 'tasks/:id' => 'tasks#show'
+
+  get 'tasks/new' => 'tasks#new'
+
+  post 'tasks' => 'tasks#create'
+
+  get 'tasks/:id/edit' => 'tasks#edit'
+
+  patch 'tasks/:id' => 'tasks#update'
+
+  delete 'tasks/:id' => 'tasks#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
